92b23cda24b1d87dd88b2a3109bb60f293fd4af6
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-05-05 20:00:33 +0530

    more cleanup

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ function App() {
     productBy: "facebook",
   });
 
-  const authToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwOGJjOGNlOTAxNjhkMTAxYTIzODMxYSIsImVtYWlsIjoia2luamFscmsyMkBnbWFpbC5jb20iLCJ1c2VyVHlwZSI6IlVTRVIiLCJpYXQiOjE2MjAxNDI5ODksImV4cCI6MTYyMDIyOTM4OX0.vnQdxFozNquh5Ix3alUQnJkWtu1SwjuzE89Y73wYU3c";
-  const jobId = "609186b901bf382c82a97015";
+  const authToken = process.env.REACT_APP_AUTH_TOKEN;
+  const paidUrl =
+    "http://localhost:5000/user/booking/609186b901bf382c82a97015/paid";
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + authToken,
+  };
 
   const makePayment = (token) => {
     const body = {
@@ -19,31 +24,22 @@ function App() {
       product,
     };
 
-    console.log("token:", token);
+    console.log("Stripe Token:", token);
 
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + authToken,
-    };
-
-    return fetch(`http://localhost:5000/user/booking/${jobId}/paid`, {
+    return fetch(paidUrl, {
       method: "POST",
       headers,
       body: JSON.stringify(body),
     })
       .then((response) => response.json())
-      .then((data) => {
-        console.log("RESPONSE ", data);
-        const { status } = data;
-        console.log("STATUS ", status);
-      })
+      .then((data) => console.log("RESPONSE ", data))
       .catch((error) => console.log(error));
   };
 
   return (
     <div className="App">
       <StripeCheckout
-        stripeKey="pk_test_51IEIaXHogLKNrbADHwvrG0alspTS89MsJwvIjGfiTpve8PmDf0ZvVgEGn6b4p2t44x9CtbYtt1RRBZNqXbYK6FtC00B21BKUKQ"
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
         token={makePayment}
         name="Buy React"
         amount={product.price * 100}
